9407e83c5eb84183afac94671ed74640fa5df291
YNNHYY <168320525+YNNHYY@users.noreply.github.com>
2024-10-23 14:53:36 +0800

    Add files via upload

---
+++ SST-LLM/models/SST-LLM.py
@@ -0,0 +1,137 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torch import optim
+
+from transformers.models.gpt2.modeling_gpt2 import GPT2Model
+from transformers import BertTokenizer, BertModel
+from einops import rearrange
+from embed import DataEmbedding, DataEmbedding_wo_time
+from transformers.models.gpt2.configuration_gpt2 import GPT2Config
+
+class moving_avg(nn.Module):
+    """
+    Moving average block to highlight the trend of time series
+    """
+    def __init__(self, kernel_size, stride):
+        super(moving_avg, self).__init__()
+        self.kernel_size = kernel_size
+        self.avg = nn.AvgPool1d(kernel_size=kernel_size, stride=stride, padding=0)
+
+    def forward(self, x):
+        # padding on the both ends of time series
+        front = x[:, 0:1, :].repeat(1, (self.kernel_size - 1) // 2, 1)
+        end = x[:, -1:, :].repeat(1, (self.kernel_size - 1) // 2, 1)
+        x = torch.cat([front, x, end], dim=1)
+        x = self.avg(x.permute(0, 2, 1))
+        x = x.permute(0, 2, 1)
+        return x
+
+class series_decomp(nn.Module):
+    """
+    Series decomposition block
+    """
+    def __init__(self, kernel_size):
+        super(series_decomp, self).__init__()
+        self.moving_avg = moving_avg(kernel_size, stride=1)
+
+    def forward(self, x):
+        moving_mean = self.moving_avg(x)
+        res = x - moving_mean
+        return res, moving_mean
+
+def compute_autocorrelation(input_data):
+    # 假设 input_data 是 (batch_size, seq_len, input_dim)
+
+    # 计算傅里叶变换
+    fft_data = torch.fft.fft(input_data, dim=1)
+
+    # 计算每个频率成分的模的平方
+    power_spectrum = torch.norm(fft_data, dim=-1)**2
+
+    # 计算逆变换得到自相关系数
+    autocorr = torch.fft.ifft(power_spectrum, dim=1).real
+
+    return autocorr
+
+class GPT4TS(nn.Module):
+    
+    def __init__(self, configs, device):
+        super(GPT4TS, self).__init__()
+        self.is_gpt = configs.is_gpt
+        self.patch_size = configs.patch_size
+        self.pretrain = configs.pretrain
+        self.stride = configs.stride
+        self.patch_num = (configs.seq_len - self.patch_size) // self.stride + 1
+
+        kernel_size = configs.kernel_size
+        self.decompsition = series_decomp(kernel_size)
+
+        self.padding_patch_layer = nn.ReplicationPad1d((0, self.stride)) 
+        self.patch_num += 1
+        
+        if configs.is_gpt:
+            if configs.pretrain:
+                self.gpt2 = GPT2Model.from_pretrained('gpt2', output_attentions=True, output_hidden_states=True)  # loads a pretrained GPT-2 base model
+            else:
+                print("------------------no pretrain------------------")
+                self.gpt2 = GPT2Model(GPT2Config())
+            self.gpt2.h = self.gpt2.h[:configs.gpt_layers]
+            print("gpt2 = {}".format(self.gpt2))
+        
+        self.in_layer = nn.Linear(configs.patch_size, configs.d_model)
+        self.out_layer = nn.Linear(configs.d_model * self.patch_num, configs.pred_len)
+        
+        if configs.freeze and configs.pretrain:
+            for i, (name, param) in enumerate(self.gpt2.named_parameters()):
+                if 'ln' in name or 'wpe' in name:
+                    param.requires_grad = True
+                else:
+                    param.requires_grad = False
+
+        for layer in (self.gpt2, self.in_layer, self.out_layer):
+            layer.to(device=device)
+            layer.train()
+        
+        self.cnt = 0
+
+
+    def forward(self, x, itr):
+        B, L, M = x.shape
+
+        means = x.mean(1, keepdim=True).detach()
+        x = x - means
+        stdev = torch.sqrt(torch.var(x, dim=1, keepdim=True, unbiased=False) + 1e-5).detach()
+        x /= stdev
+
+        seasonal_init, trend_init = self.decompsition(x)
+
+        auto = compute_autocorrelation(x).unsqueeze(-1)
+        auto_x = x * auto
+
+        auto_means = auto_x.mean(1, keepdim=True).detach()
+        auto_x = auto_x - auto_means
+        auto_stdev = torch.sqrt(torch.var(auto_x, dim=1, keepdim=True, unbiased=False) + 1e-5).detach()
+        auto_x /= auto_stdev
+
+        x = 2*x + auto_x + trend_init
+
+
+        x = rearrange(x, 'b l m -> b m l')
+
+        x = self.padding_patch_layer(x)
+        x = x.unfold(dimension=-1, size=self.patch_size, step=self.stride)
+        x = rearrange(x, 'b m n p -> (b m) n p')
+
+        outputs = self.in_layer(x)
+
+        if self.is_gpt:
+            outputs = self.gpt2(inputs_embeds=outputs).last_hidden_state
+
+        outputs = self.out_layer(outputs.reshape(B*M, -1))
+        outputs = rearrange(outputs, '(b m) l -> b l m', b=B)
+
+        outputs = outputs * stdev
+        outputs = outputs + means
+
+        return outputs
